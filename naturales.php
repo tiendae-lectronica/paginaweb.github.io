265ea647ecf56dc186eb590c5528f32584dbfa27
tiendae-lectronica <jonathanguardado096@gmail.com>
2024-10-27 19:35:51 -0600

    Add files via upload

---
+++ naturales.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Standar Historia</title>
+    <link rel="stylesheet" href="estilos.css">
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="main.js"></script>
+</head>
+<Style>
+#principal
+{
+border:0;
+margin: 0 auto;
+width: 75%;
+padding: 7px;
+margin-bottom: 9px
+}
+body
+{
+background: url(Standard/bananos.jpg);
+background-size: 100%;
+background-attachment: fixed;
+}
+</Style>
+<body>
+ <div id="principal">
+		      </div>
+	<header>
+	<div class="todo-fixed">
+         <div class="contenedor" id="uno">
+         <a href="inicio.html"><img class="icon" src="iconos/inicio.png" ></a>
+         <p class="texto">Inicio</p>
+         </div>
+
+          <div class="contenedor" id="dos">
+          <a href="productos.html"><img class="icon" src="iconos/programacion.png"></a>
+          <p class="texto">productos</p>
+         </div>
+         
+
+          <div class="contenedor" id="tres">
+          <a href="galeria.html"><img class="icon" src="iconos/galeria.png"></a>
+          <p class="texto">Galeria</p>
+         </div>
+         
+
+          <div class="contenedor" id="cuatro">
+          <a href="contactenos.html"><img class="icon" src="iconos/contactenos.png"></a>
+          <p class="texto">Contactenos</p>
+         </div>
+        </div>
+	</header>
+    <tittle></tittle>
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="fonts.css">
+</head>
+<body>
+	<div class="social">
+		<ul>
+			<li><a href="https://www.facebook.com/jeison.sarmiento.148" target="_blank" class="icon-facebook"></a></li>
+			<li><a href="http://www.twitter.com/falconmasters" target="_blank" class="icon-twitter"></a></li>
+			<li><a href="https://plus.google.com/?hl=es" target="_blank" class="icon-google"></a></li>
+			<li><a href="http://www.pinterest.com/falconmasters" target="_blank" class="icon-pinterest"></a></li>
+			<li><a href="https://www.youtube.com/results?search_query=futbol" target="_blank" class="icon-youtube"></a></li>
+		</ul>
+	</div>
+	
+</body>
+<img src="../../Documentos/Sitio sin nombre 2/Imagen de WhatsApp 2024-10-10 a las 11.41.30_a897cbfe.jpg" width="1648" height="400">
+   <div class="documento">
+    <h5><marquee>Standar</marquee></h5>
+       <p><h2>
+       	 Breve resumen 
+       	</p></h2>
+       	<p><h4><br>
+Standar a sido una de las empresas con mayor beneficio para la poblacion hondureña ya que abeneficiado a muchos hondeureños
+</p>
+<p><br>
+con el transcurso de el tiempo estandar se a convertido en una de las empresas mas importante para el pais ya que en ella trabajan miles de personas
+</p></h4>
+<p> <h2><br>
+<h1><marquee>Historia de Standar</marquee></h1>
+</p></h2>
+<p><h4><br>
+Durante la primera mitad de el siglo xx la economia de Honduras fue dominada por empresas estado unidenses como la Standarfruit company 
+la cual  establecio enorme plantaciones de banano a lo largo de la costa norte esta empresa iso rapidamente la exportacion de el producto con virtiendolo en uno de los mas exportados de el pais
+De esta forma, el capital extranjero, la vida en las plantaciones de banano, y los conservadores fueron factores determinantes en la politica de hondurasdesde 1988 hasta mediados de el siglo xx.
+
+</p>
+<p><br>
+Maanuel bonilla llego ala precidencia de Honduras , por  medio de las armas . una vez en el poder resulto hacer mejor amigo de las compañias bananeras que su antecesor, Terencio Sierra. Durante su administracion
+estas empresas ganaron las extensiones de impuestos y el permiso para la construccion de muelles y carreteras asi como el permiso para obtener las cartas para la construccion del nuevo ferrocarril nacional de Honduras.
+</p>
+<p><br>
+Durante la administracion de Miguel Rafael Davila Cuellar, Estados Unidos trato de ponerle fin a los conflictos centroamericanos atravez del tratado de paz y amistad centroamericana
+de 1907. Sin embargo, de 1920 a 1923 le dieron 17 levantamiento o intentos de golpes de estado en Honduras.</p></h4>
+   </div>    
+</html>
+
+
+
+
+
+
+
+
+
+
+
